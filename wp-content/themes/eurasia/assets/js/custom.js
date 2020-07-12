493234ad186e7731e895333b9611fd0e2d0e1d0c
mortalshot <seregagl1996@gmail.com>
2020-07-12 21:41:46 +0300

    аякс добавление в корзину

--- wp-content/themes/eurasia/assets/js/custom.js
+++ wp-content/themes/eurasia/assets/js/custom.js
@@ -230,4 +230,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // !Маска на ввод номера телефона
     $("#billing_phone").mask("+7(999) 999-9999");
+
+
+    $('.custom_add_to_cart').click(function (e) {
+        e.preventDefault();
+        var id = $(this).next().next().next().attr('value');
+        var data = {
+            product_id: id,
+            quantity: 1
+        };
+        $(this).parent().addClass('loading');
+        $.post(wc_add_to_cart_params.wc_ajax_url.toString().replace('%%endpoint%%', 'add_to_cart'), data, function (response) {
+
+            if (!response) {
+                return;
+            }
+            if (response.error) {
+                alert("Custom Massage ");
+                $('.custom_add_to_cart').parent().removeClass('loading');
+                return;
+            }
+            if (response) {
+
+                var url = woocommerce_params.wc_ajax_url;
+                url = url.replace("%%endpoint%%", "get_refreshed_fragments");
+                $.post(url, function (data, status) {
+                    $(".woocommerce.widget_shopping_cart").html(data.fragments["div.widget_shopping_cart_content"]);
+                    if (data.fragments) {
+                        jQuery.each(data.fragments, function (key, value) {
+
+                            jQuery(key).replaceWith(value);
+                        });
+                    }
+                    jQuery("body").trigger("wc_fragments_refreshed");
+                });
+                $('.custom_add_to_cart').parent().removeClass('loading');
+
+            }
+        });
+    });
 });
--- wp-content/themes/eurasia/functions.php
+++ wp-content/themes/eurasia/functions.php
@@ -130,23 +130,14 @@ function iconic_loop_variation_add_to_cart_button()
 	global $product;
 ?>
 	<div class="woocommerce-variation-add-to-cart variations_button">
-		<button type="submit" class="single_add_to_cart_button button ajax_add_to_cart"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+		<button type="submit" class="custom_add_to_cart single_add_to_cart_button button ajax_add_to_cart"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+
 		<input type="hidden" name="add-to-cart" value="<?php echo absint($product->get_id()); ?>" />
 		<input type="hidden" name="product_id" value="<?php echo absint($product->get_id()); ?>" />
 		<input type="hidden" name="variation_id" class="variation_id" value="0" />
 	</div>
 <?php
 }
-
-// Удаление редиректа на страницу продукта
-function iconic_add_to_cart_form_action($redirect)
-{
-	if (!is_archive() && !is_front_page()) {
-		return $redirect;
-	}
-	return '';
-}
-add_filter('woocommerce_add_to_cart_form_action', 'iconic_add_to_cart_form_action');
 
 // !cart.php
 /* Изменение названий полей */
